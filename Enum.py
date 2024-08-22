848225d59eba1b7ef706270a5ff9c4df603f0546
KalebYigezu <43226925+KalebYigezu@users.noreply.github.com>
2024-08-22 16:57:14 -0700

    Update Enum.py

--- Enum.py
+++ Enum.py
@@ -1,1 +1,30 @@
+#from enum import Enum
+characters = ["Kaleb", "Getu", "Veronica", "Goytom", "Paulos"]
+print("\n------------------------------------\n")
+
+print(list(enumerate(characters)))
+print("\n------------------------------------\n")
+
+for index, character in enumerate(characters):
+    print(index, character)
+
+print("\n------------------------------------\n")
+
+characters = ["Kaleb", "Getu", "Getu", "Goytom", "Paulos",
+              "Kaleb", "Getu", "Veronica", "Goytom","Paulos",
+              "Paulos", "Getu", "Veronica", "Getu", "Paulos"]
+
+character_map = {character:[] for character in set(characters)}
+
+print(character_map)
+print("\n------------------------------------\n")
+print(list(enumerate(characters)))
+print("\n------------------------------------\n")
+
+
+for index, character in enumerate(characters):
+    character_map[character].append(index)
+
+print(character_map)
+print("\n------------------------------------\n")
 
